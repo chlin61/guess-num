554f23a80af25c7aa4ed59ad8f99aed9fce6cc9b
Sean Lin <58102391+chlin61@users.noreply.github.com>
2019-11-28 23:53:50 +0800

    update for guess number

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import random
 ran = random.randint(1, 100)
-
+count = 0
 while True:
 	insert_ran = int(input('請輸入要猜的數字: '))
+	count = count + 1
 	if ran == insert_ran:
-		print('你猜對了')
+		print('你猜對了,你猜了', count,'次')
 		break
 	elif ran > insert_ran:
 		print('你的答案要在更大一點喔')
